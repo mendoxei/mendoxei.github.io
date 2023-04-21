4c8bd92c50d43e129272e8d4eefc22f18be3a48c
Lancelot <lanle@hotmail.nl>
2023-04-21 22:49:08 +0200

    video embeds, styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,38 @@ import { Routes, Route } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import RandomKanji from './RandomKanji'
 import Home from './Home'
+import { Avatar, Box, Divider } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2' // Grid version 2
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello World</h1>
-      <p>I'm hosted with GitHub Pages.</p>
-      <div>
-        <Link to='/'>Home </Link>
-        <Link to='/kanji'>Kanji</Link>
-      </div>
+      <Box sx={{ width: '100%', backgroundColor: 'turquoise' }}>
+        <Grid
+          container
+          spacing={10}
+          direction='row'
+          justifyContent='center'
+          alignItems='center'
+        >
+          <Grid xs={0}>
+            <Avatar
+              alt='mendoxei'
+              src='fix.PNG'
+              sx={{ width: '10vh', height: '10vh', margin: 2 }}
+            />
+          </Grid>
+          <Grid xs={3}>
+            <h1 style={{ color: 'yellow', fontSize: '5vh' }}>MENDOXEI</h1>
+          </Grid>
+        </Grid>
+      </Box>
+      <Grid spacing={0} direction='column' alignItems='center'>
+        <Box sx={{ width: '100%', backgroundColor: 'lightgray' }}>
+          <Link to='/'>Home </Link>
+          <Link to='/kanji'>Kanji</Link>
+        </Box>
+      </Grid>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/kanji' element={<RandomKanji />} />
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import { Avatar, Box, Grid } from '@mui/material'
 
 const Home = () => {
   return (
     <div>
-      Home
-      <p>
-        <a href='https://www.youtube.com/watch?v=oTy7rO4hEvU'>
-          Gametechnologie Introductieproject trailer: Noxium
-        </a>
-      </p>
-      <p>
-        <a href='https://www.youtube.com/watch?v=6C1cRpy4b44'>
-          FairRecKit demo/trailer
-        </a>
-      </p>
+      <Box sx={{ backgroundColor: 'white' }}>
+        <Grid
+          container
+          spacing={2}
+          alignItems='center'
+          direction='column'
+          justifyContent='center'
+          style={{ width: '50%', margin: 'auto' }}
+        >
+          <p>
+            <h3>Gametechnologie Introductieproject trailer: Noxium</h3>
+            <iframe
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/oTy7rO4hEvU'
+              title='YouTube video player'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            ></iframe>
+          </p>
+          <p>
+            <h3>FairRecKit demo/trailer</h3>
+            <iframe
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/6C1cRpy4b44'
+              title='YouTube video player'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            ></iframe>
+          </p>
+        </Grid>
+      </Box>
     </div>
   )
 }
