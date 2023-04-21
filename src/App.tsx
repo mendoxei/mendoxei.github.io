ab27b4f31f3d8bd6e56dd551f6f9fb83a2ae332f
Lancelot <lanle@hotmail.nl>
2023-04-21 18:06:44 +0200

    create react app start

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello World</h1>
+    <p>I'm hosted with GitHub Pages.</p>
+      <a href="https://www.youtube.com/watch?v=oTy7rO4hEvU"
+      >Gametechnologie Introductieproject trailer: Noxium</a
+    >
+    <a href="https://www.youtube.com/watch?v=6C1cRpy4b44"
+      >FairRecKit demo/trailer</a
+    >
+    </div>
+  );
+}
+
+export default App;
